e2df43ee7d3a5f847204e3239559fe21443de5d9
Robiee1 <rroobbee13@gmail.com>
2021-08-01 10:34:57 +0200

    moved the service in to main config file and remove bug

--- config.js
+++ config.js
@@ -4,19 +4,13 @@ const Discord = require('discord.js');
 const prefix = 'slave ';
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const fs = require('fs');
-const channel = '867493610353459211';
+const channelId = process.env.DISCORDJS_CHANNEL_ID;
 
 
 client.commands = new Discord.Collection();
 client.services = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 const serviceFiles = fs.readdirSync('./service/').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
 
 for (const file of serviceFiles) {
     const service = require(`./service/${file}`);
@@ -28,19 +22,43 @@ client.on('ready', () => {
 
 });
 
-client.on('messageReactionAdd', message => {
-
-})
-
 client.on('message', message => {
-
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLocaleLowerCase();
-
-
     if (command === 'reactionservice') {
-        client.services.get('reactionService').execute(message, args, Discord, client, channel);
+        client.services.get('reactionService').execute(message, args, Discord, client, channelId);
+    }
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    const monkaRole = reaction.message.guild.roles.cache.find(role => role.name === "MONKA");
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.message.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+
+    if (reaction.message.channel.id == channelId) {
+        if (reaction.emoji.name === 'YEP') {
+            await reaction.message.guild.members.cache.get(user.id).roles.add(monkaRole);
+        }
+    } else {
+        return;
+    }
+});
+client.on('messageReactionRemove', async (reaction, user) => {
+    const monkaRole = reaction.message.guild.roles.cache.find(role => role.name === "MONKA");
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.message.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+
+    if (reaction.message.channel.id == channelId){
+        if(reaction.emoji.name === 'YEP'){
+            await reaction.message.guild.members.cache.get(user.id).roles.remove(monkaRole);
+        }
+    } else {
+        return;
     }
 });
 
