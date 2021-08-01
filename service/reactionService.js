aa2d4cf0d7c370d732dfac47b9f8085d8ae6e4f3
Robiee1 <rroobbee13@gmail.com>
2021-08-01 10:36:17 +0200

    remove reactionListeners from reactionService

--- service/reactionService.js
+++ service/reactionService.js
@@ -1,9 +1,7 @@
 module.exports = {
     name: 'reactionService',
     description: 'Gives a role on a message reaction',
-    async execute(message, args, Discord, client, channel){
-
-        const monkaRole = message.guild.roles.cache.find(role => role.name === "MONKA");
+    async execute(message, args, Discord){
 
         const monkaEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'YEP') ;
 
@@ -14,37 +12,5 @@ module.exports = {
 
         let messageEmbed = await message.channel.send(embed);
         messageEmbed.react(monkaEmoji);
-
-
-        client.on('messageReactionAdd', async (reaction, user) => {
-            if (reaction.message.partial) await reaction.message.fetch();
-            if (reaction.partial) await reaction.message.fetch();
-            if (user.bot) return;
-            if (!reaction.message.guild) return;
-
-            if (reaction.message.channel.id == channel){
-                if(reaction.emoji.name === 'YEP'){
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(monkaRole);
-                }
-            } else {
-                return;
-            }
-        });
-        client.on('messageReactionRemove', async (reaction, user) => {
-            if (reaction.message.partial) await reaction.message.fetch();
-            if (reaction.partial) await reaction.message.fetch();
-            if (user.bot) return;
-            if (!reaction.message.guild) return;
-
-            if (reaction.message.channel.id == channel){
-                console.log('was here');
-                if(reaction.emoji.name === monkaEmoji){
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(monkaRole);
-                }
-            } else {
-                return;
-            }
-        });
-
     }
 }
